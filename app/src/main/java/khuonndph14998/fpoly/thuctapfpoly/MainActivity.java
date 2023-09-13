4121984cefc2d502e9635e79cff12fe449415ca8
Khương Nguyễn Đa <dakhuong281202@gmail.com>
2023-09-13 11:11:19 +0700

    Initial commit

---
+++ app/src/main/java/khuonndph14998/fpoly/thuctapfpoly/MainActivity.java
@@ -0,0 +1,14 @@
+package khuonndph14998.fpoly.thuctapfpoly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
